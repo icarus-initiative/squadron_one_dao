461d2697998a190a9badeed1bb10d6c13c6e0799
timOTdev <tim@timothyhoang.dev>
2021-12-01 19:26:27 -0600

    Update whitelist and tests

--- programs/first_squadron_dao/src/lib.rs
+++ programs/first_squadron_dao/src/lib.rs
@@ -7,9 +7,24 @@ pub mod first_squadron_dao {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         let proposal_account = &mut ctx.accounts.proposal_account;
+
+        // let whitelist = vec![
+        //     "AuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string(),
+        //     "BuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string(),
+        //     "CuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string(),
+        //     "DuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string(),
+        //     "EuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string(),
+        //     "FuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string()
+        // ];
+
+        // let whitelist = "AuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3,BuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3,CuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3,DuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3,EuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3,FuzqjpoMTRp778HYDrbtYGuM95CzwiNMGwh4hmtWkD3".to_string();
+        // proposal_account.whitelist = whitelist;
+        
         proposal_account.yay = 0;
         proposal_account.nay = 0;
         proposal_account.total = 0;
+        proposal_account.whitelist = "adam,bob,cathy,frank,harry,jack".to_string();
+
         Ok(())
     }
 
@@ -26,15 +41,11 @@ pub mod first_squadron_dao {
         proposal_account.total += 1;
         Ok(())
     }
-
-    pub fn count_votes(_ctx: Context<Vote>) -> ProgramResult {
-        Ok(())
-    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 16 + 16)]
+    #[account(init, payer = user, space = 256)]
     pub proposal_account: Account<'info, ProposalAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -52,13 +63,5 @@ pub struct ProposalAccount {
     pub yay: u8,
     pub nay: u8,
     pub total: u8,
-}
-
-pub struct Pilots {
-    pub first: u8,
-    pub second: u8,
-    pub third: u8,
-    pub fourth: u8,
-    pub fifth: u8,
-    pub sixth: u8,
+    pub whitelist: String
 }
--- tests/first_squadron_dao.js
+++ tests/first_squadron_dao.js
@@ -7,82 +7,147 @@ describe("first_squadron_dao", () => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.FirstSquadronDao;
-  const proposalAccount = anchor.web3.Keypair.generate();
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Hard coded whitelist.
+  const whitelist = "adam,bob,cathy,frank,harry,jack";
 
   it("Initializes with 0 votes for yay and nay", async () => {
-    console.log("Testing Initialize...");
     /* The last element passed to RPC methods is always the transaction options. Because proposal is being created here, we are required to pass it as a signers array */
     const tx = await program.rpc.initialize({
       accounts: {
-        proposalAccount: proposalAccount.publicKey,
+        proposalAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       },
-      signers: [proposalAccount],
+      signers: [baseAccount],
     });
     const account = await program.account.proposalAccount.fetch(
-      proposalAccount.publicKey
+      baseAccount.publicKey
     );
     
-    console.log("Your transaction signature", tx);
-    console.log("Yay: ", account.yay.toString());
-    console.log("Nay: ", account.nay.toString());
-    assert.ok(
-      account.yay.toString() == 0 && account.nay.toString() == 0
-    );
+    console.log("\n");
+    console.log("your transaction signature", tx);
+    console.log("yay: ", account.yay.toString());
+    console.log("nay: ", account.nay.toString());
+    console.log("test: account.yay.toString() == 0 && account.nay.toString() == 0");
+
+    assert.ok(account.yay.toString() == 0 && account.nay.toString() == 0);
   });
 
   it("Votes correctly voteYay", async () => {
-    console.log("Testing voteYay...");
     await program.rpc.voteYay({
       accounts: {
-        proposalAccount: proposalAccount.publicKey,
+        proposalAccount: baseAccount.publicKey,
       },
     });
-    const account = await program.account.proposalAccount.fetch(
-      proposalAccount.publicKey
-    );
-    console.log("Yay: ", account.yay.toString());
-    console.log("Nay: ", account.nay.toString());
-    assert.ok(
-      account.yay.toString() == 1 && account.nay.toString() == 0
-    );
+    const account = await program.account.proposalAccount.fetch(baseAccount.publicKey);
+
+    console.log("\n");
+    console.log("yay: ", account.yay.toString());
+    console.log("nay: ", account.nay.toString());
+    console.log("test: account.yay.toString() == 1 && account.nay.toString() == 0");
+
+    assert.ok(account.yay.toString() == 1 && account.nay.toString() == 0);
   });
 
   it("Votes correctly voteNay", async () => {
-    console.log("Testing voteNay...");
     await program.rpc.voteNay({
       accounts: {
-        proposalAccount: proposalAccount.publicKey,
+        proposalAccount: baseAccount.publicKey,
       },
     });
-    const account = await program.account.proposalAccount.fetch(
-      proposalAccount.publicKey
-    );
-    console.log("Yay: ", account.yay.toString());
-    console.log("Nay: ", account.nay.toString());
-    assert.ok(
-      account.yay.toString() == 1 && account.nay.toString() == 1
-    );
+    const account = await program.account.proposalAccount.fetch(baseAccount.publicKey);
+
+    console.log("\n");
+    console.log("yay: ", account.yay.toString());
+    console.log("nay: ", account.nay.toString());
+    console.log("test: account.yay.toString() == 1 && account.nay.toString() == 1");
+
+    assert.ok(account.yay.toString() == 1 && account.nay.toString() == 1);
   });
 
   it("Counts total votes", async () => {
-    console.log("Testing countVotes...");
-    await program.rpc.countVotes({
-      accounts: {
-        proposalAccount: proposalAccount.publicKey,
-      },
-    });
     const account = await program.account.proposalAccount.fetch(
-      proposalAccount.publicKey
+      baseAccount.publicKey
     );
     const tally = (account.yay + account.yay).toString();
     const total = account.total.toString();
-    console.log("Tally: ", tally);
-    console.log("Total Votes: ", total);
+
+    console.log("\n");
+    console.log("tally: ", tally);
+    console.log("total votes: ", total);
+    console.log("test: tally == total");
     
-    assert.ok(
-      tally == total
+    assert.ok(tally == total);
+  });
+
+  it("Initializes with a hard-coded whitelist", async () => {
+    const account = await program.account.proposalAccount.fetch(
+      baseAccount.publicKey
     );
-  })
+
+    account.whitelist = whitelist;
+    
+    console.log("\n");
+    console.log("whitelist: ", account.whitelist);
+    console.log("test: account.whitelist.length > 0");
+
+    assert.ok(account.whitelist.length > 0);
+  });
+
+  it("Initializes with 6 members exactly", async () => {
+    const account = await program.account.proposalAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    account.whitelist = whitelist;
+    const cleaved = account.whitelist.split(',');
+    const whitelist_length = cleaved.length;
+    
+    console.log("\n");
+    console.log("whitelist: ", account.whitelist);
+    console.log("whitelist_length: ", whitelist_length);
+    console.log("test: whitelist_length === 6");
+
+    assert.ok(whitelist_length === 6);
+  });
+
+  it("Matches on whitelist", async () => {
+    const account = await program.account.proposalAccount.fetch(baseAccount.publicKey);
+
+    account.whitelist = whitelist;
+    const cleaved = whitelist.split(',');
+
+    const target = "frank";
+    const found = cleaved.indexOf(target);
+    
+    console.log("\n");
+    console.log("whitelist: ", whitelist);
+    console.log("target: ", target);
+    console.log("found: ", found);
+    console.log("test: found >= 0");
+
+    assert.ok(found >= 0);
+  });
+
+  it("Matches none on whitelist", async () => {
+    const account = await program.account.proposalAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    account.whitelist = whitelist;
+    const cleaved = whitelist.split(',');
+
+    const target = "anatoly";
+    const found = cleaved.indexOf(target);
+
+    console.log("\n");
+    console.log("whitelist: ", whitelist);
+    console.log("target: ", target);
+    console.log("found: ", found);
+    console.log("test: found < 0");
+
+    assert.ok(found < 0);
+  });
 });
