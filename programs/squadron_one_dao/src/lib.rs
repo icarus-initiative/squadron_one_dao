c8bdfc38aee92e0e260ea758aa4fa59e3c2c865b
timOTdev <tim@timothyhoang.dev>
2021-12-21 22:44:14 -0600

    Update anchor code

--- programs/squadron_one_dao/src/lib.rs
+++ programs/squadron_one_dao/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("HRtvmXDXyGRLU9sVCuMY3x99f3p1NT1p28Fur4ZhH3iz");
+declare_id!("GtT93NhhvnWu6LWL7yNeHjqLehNgGZeTcJLxV74eNjWm");
 
 #[program]
 pub mod squadron_one_dao {
@@ -10,36 +10,36 @@ pub mod squadron_one_dao {
     // Initialize Function
     // =====================
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        // Initialize the main state.
-        let main_state = &mut ctx.accounts.main_state;
+        // Initialize the base account.
+        let base_account = &mut ctx.accounts.base_account;
 
-        // Set the authority on the main state as the account that starts the Dapp.
-        main_state.authority = ctx.accounts.authority.key();
+        // Set the authority on the base account as the account that starts the Dapp.
+        base_account.authority = ctx.accounts.authority.key();
 
         Ok(())
     }
 
     // =====================
-    // Primary Functions
+    // Authorized Functions
     // =====================
     pub fn add_whitelist(
         ctx: Context<AuthorityContext>,
         p_pilots: Vec<Pubkey>
     ) -> ProgramResult {
-        // Initialize the main state.
-        let main_state = &mut ctx.accounts.main_state;
+        // Initialize the base account.
+        let base_account = &mut ctx.accounts.base_account;
 
-        // Loop over the vector of p_pilots sent with call.
+        // Loop over the vector of p_pilots payload from request.
         for p_address in p_pilots.iter() {
-            // Boolean to track if p_pilot already exists on main_state.
+            // Boolean to track if p_pilot already exists on whitelist.
             let mut exists = false;
 
-            // Loop through the pilots vector in the main state.
-            for (index, pilot) in main_state.whitelist.iter().enumerate() {
-                // If the main_state pilot matches the p_pilot,
+            // Loop through the pilots vector in the base account.
+            for (index, pilot) in base_account.whitelist.iter().enumerate() {
+                // If the base_account pilot matches the p_pilot,
                 if pilot.address == *p_address {
                     // Mark them as a voter in case they were marked inactive.
-                    main_state.whitelist[index].active = true;
+                    base_account.whitelist[index].active = true;
 
                     // Flag that the p_pilot already exists.
                     exists = true;
@@ -49,10 +49,9 @@ pub mod squadron_one_dao {
                 }
             }
 
-            // If the p_pilot didn't exist in the main_state,
+            // Add the pilot if the p_pilot didn't exist in the base_account.
             if !exists {
-                // Add the pilot to the main state with the Pilot struct.
-                main_state.whitelist.push(Pilot {
+                base_account.whitelist.push(Pilot {
                     address: *p_address,
                     active: true,
                 })
@@ -62,59 +61,82 @@ pub mod squadron_one_dao {
         Ok(())
     }
 
+    pub fn mark_inactive(
+        ctx: Context<AuthorityContext>,
+        p_pilots: Vec<Pubkey>
+    ) -> ProgramResult {
+        // Initialize the base account.
+        let whitelist = &mut ctx.accounts.base_account.whitelist;
+
+        // Loop over the vector of p_pilots payload from request.
+        for i in 0..p_pilots.len() {
+            // Then loop over the whitelist addresses concurrently.
+            for j in 0..whitelist.len() {
+                // We only want to target the active ones and
+                // mark inactive only if the addresses is specified in the request.
+                if whitelist[j].active && (p_pilots[i] == whitelist[j].address){
+                    // Marks user as inactive.
+                    whitelist[j].active = false;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    // =====================
+    // Unauthorized Functions
+    // =====================
     pub fn vote(
         ctx: Context<VoterContext>,
         p_choice: u8
     ) -> ProgramResult {
-        // Initialize the main state.
-        let main_state = &mut ctx.accounts.main_state;
+        // Initialize the base account.
+        let base_account = &mut ctx.accounts.base_account;
 
         // Capture the voter's pubkey.
         let p_voter = ctx.accounts.voter.key();
 
-        // Tracker if person is allowed to vote;
+        // Tracker if person is allowed to vote.
         let mut can_vote = false;
 
         // Check if the address is on the whitelist.
-        for pilot in main_state.whitelist.iter() {
-            if pilot.address == p_voter {
+        for pilot in base_account.whitelist.iter() {
+            if pilot.active && (pilot.address == p_voter) {
                 can_vote = true;
             }
         }
 
-        // If they are not flagged as allowed, return unit.
+        // This person is allowed to vote.
         if can_vote {
             // Tracker if the pubkey has already voted.
             let mut has_voted = false;
 
-            // Loop through the votes vector.
             // If the Pubkey addresses match, flag p_voter already voted.
-            for vote in main_state.voters.iter() {
+            for vote in base_account.votelist.iter() {
                 if vote.address == p_voter {
                     has_voted = true;
+                    msg!("Already vote.");
                     break;
                 }
             }
 
             // If p_voter has not voted, tally their vote.
             if !has_voted {
-                main_state.voters.push(Voter {
+                base_account.votelist.push(Voter {
                     address: p_voter,
                     choice: p_choice,
                 });
 
                 if p_choice == 0 {
-                    main_state.nay += 1;
+                    base_account.nay += 1;
                 }
                 else if p_choice == 1 {
-                    main_state.yay += 1;
+                    base_account.yay += 1;
                 }
             }
-            else {
-                msg!("Already vote.")
-            }
         } else {
-            msg!("Voting permission denied.")
+            msg!("Voting permission denied.");
         }
 
         Ok(())
@@ -131,10 +153,10 @@ pub struct Initialize<'info> {
         payer = authority,
         space = 8 // Account Discriminator
         + 32 // Authority Pubkey
-        + 256 // For 6 pubkeys (6 * 32)
+        + 320 // For 10 pubkeys (10 * 32)
         + 1024 // For good measure
     )]
-    pub main_state: Account<'info, MainState>,
+    pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -146,14 +168,14 @@ pub struct Initialize<'info> {
 #[derive(Accounts)]
 pub struct AuthorityContext<'info> {
     #[account(mut, has_one = authority)]
-    pub main_state: Account<'info, MainState>,
+    pub base_account: Account<'info, BaseAccount>,
     pub authority: Signer<'info>
 }
 
 #[derive(Accounts)]
 pub struct VoterContext<'info> {
     #[account(mut)]
-    pub main_state: Account<'info, MainState>,
+    pub base_account: Account<'info, BaseAccount>,
     pub voter: Signer<'info>
 }
 
@@ -174,10 +196,10 @@ pub struct Voter {
 
 #[account]
 #[derive(Default)]
-pub struct MainState {
+pub struct BaseAccount {
     authority: Pubkey,
     pub whitelist: Vec<Pilot>,
-    pub voters: Vec<Voter>,
+    pub votelist: Vec<Voter>,
     pub yay: u8,
     pub nay: u8,
 }
--- tests/squadron_one_dao.ts
+++ tests/squadron_one_dao.ts
@@ -6,25 +6,20 @@ describe('squadron_one_dao', () => {
   // Initialize Setup
   // ====================================================================================
   // We set up the provider from the wallet in our OS system folder.
-  // That's in ~/Users/USERNAME/.config/solana/id.json.
-  // We create the mainstate Keypair for our pgoram.
-  // We make 2 whitelist Pubkeys and a whitelist.
+  // That file is located in ~/Users/USERNAME/.config/solana/id.json.
 
-  // Configure the client to use the local cluster.
+  // Configure the provider wallet.
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.SquadronOneDao;
 
   // Create Keypair for our program.
-  const mainState = anchor.web3.Keypair.generate();
+  const baseAccount = anchor.web3.Keypair.generate();
 
   // Make 2 whitelist public keys.
-  // Binary 782a639ad0537be95ab2d7c053ad031c93b259dc755d6c1dc510d0c3a15160a9
   const whitelisted1 = new PublicKey(
     '965QwptcVkD7otW4bxHeDrWoXMcbGzyGqbAUjcjvfPwN'
   );
-
-  // Binary e96cb05d27d7cdbb9c97f4b080ad99684eacb095532ba93e43045eaf533708c
   const whitelisted2 = new PublicKey(
     'yx54BueUCTurDDYMMpfWYLDchkZiEDCf9gMKjQ6dYsD'
   );
@@ -43,14 +38,14 @@ describe('squadron_one_dao', () => {
     // Add your test here.
     const tx = await program.rpc.initialize({
       accounts: {
-        mainState: mainState.publicKey,
+        baseAccount: baseAccount.publicKey,
         authority: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       },
-      signers: [mainState],
+      signers: [baseAccount],
     });
 
-    console.log('Your transaction signature', tx);
+    console.log('Your transaction signature:', tx);
 
     // providerWallet
     // binary a650147383b2f0efab54055c68811551517b1a5985ed044d23bf8e5254584283
@@ -58,11 +53,11 @@ describe('squadron_one_dao', () => {
     console.log('providerWallet binary:', provider.wallet.publicKey);
     console.log('providerWallet b58:', provider.wallet.publicKey.toBase58());
 
-    // mainState
+    // baseAccount
     // binary 3c2cef3728d6809b30610ee4faf87f16164e82f64f7c42902355e2d202284247
     // b58 53uCitj3jBmGKPnnBdawjxCSVya9KJLJqn7eLHqNdih4
-    console.log('mainState binary:', mainState.publicKey);
-    console.log('mainState b58:', mainState.publicKey.toBase58());
+    console.log('baseAccount binary:', baseAccount.publicKey);
+    console.log('baseAccount b58:', baseAccount.publicKey.toBase58());
 
     // whitelisted1
     // binary 782a639ad0537be95ab2d7c053ad031c93b259dc755d6c1dc510d0c3a15160a9
@@ -76,8 +71,12 @@ describe('squadron_one_dao', () => {
     console.log('whitelisted2 binary:', whitelisted2);
     console.log('whitelisted2 b58:', whitelisted2.toBase58());
 
-    const account = await program.account.mainState.fetch(mainState.publicKey);
+    // Grab the baseAccount.
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
 
+    // Check that the authority was set to the provider wallet.
     assert.ok(
       account.authority.toString() == provider.wallet.publicKey.toString()
     );
@@ -91,34 +90,40 @@ describe('squadron_one_dao', () => {
     console.log('========================');
     await program.rpc.addWhitelist(p_whitelist, {
       accounts: {
-        mainState: mainState.publicKey,
+        baseAccount: baseAccount.publicKey,
         authority: provider.wallet.publicKey,
       },
     });
 
-    const account = await program.account.mainState.fetch(mainState.publicKey);
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
 
     console.log('whitelist', account.whitelist);
 
+    // The list we provided should match the length on the baseAccount.
     assert.ok(account.whitelist.length == p_whitelist.length);
   });
 
   // Test 3: Checking Whitelist Additions
   // ====================================================================================
   // We just check that whitelist1 Publickey was added to the whitelist.
-  it('Vector contains whitelist member: 965-PwN', async () => {
+  it('Vector contains whitelist member: 965...PwN', async () => {
     console.log('========================');
     const pilot = new PublicKey(
       '965QwptcVkD7otW4bxHeDrWoXMcbGzyGqbAUjcjvfPwN'
     ).toBase58();
 
-    const account = await program.account.mainState.fetch(mainState.publicKey);
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
 
     const found = account.whitelist.filter(
       (item) => item.address.toBase58() === pilot
     );
 
-    console.log('found', found[0].address.toBase58());
+    console.log('Whitelisted:', found);
+    console.log('Whitelisted address b58:', found[0].address.toBase58());
 
     assert.ok(found.length > 0);
   });
@@ -131,82 +136,118 @@ describe('squadron_one_dao', () => {
     console.log('========================');
     await program.rpc.vote(1, {
       accounts: {
-        mainState: mainState.publicKey,
+        baseAccount: baseAccount.publicKey,
         voter: provider.wallet.publicKey,
       },
     });
 
-    const account = await program.account.mainState.fetch(mainState.publicKey);
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
 
-    console.log('Yays', account.yay);
-    console.log('Nays', account.nay);
+    console.log('Yays:', account.yay);
+    console.log('Nays:', account.nay);
+
+    // A non-whitelisted member's vote will not be recorded.
     assert.ok(account.yay == 0);
   });
 
   // Test 5: Voting yay as Whitelisted
   // ====================================================================================
-  // We have to add the provider to whitelist.
-  // With allowed permission, the vote will get recorded.
+  // First, we have to add the provider to whitelist.
+  // Subsequently, the permission is granted and the vote will get recorded.
   // NOTE: We can't change the provider.wallet after we started test suite.
   // Therefore, we must test using the provider as the whitelisted member.
-  // AKA, one can't send their vote in from another wallet address, at least in the test suite.
-  it('Member is whitelist and votes yay', async () => {
+  // AKA, we can't send a vote in from another wallet address, at least in the test suite.
+  it('Member added whitelist and votes yay', async () => {
     console.log('========================');
     await program.rpc.addWhitelist([provider.wallet.publicKey], {
       accounts: {
-        mainState: mainState.publicKey,
+        baseAccount: baseAccount.publicKey,
         authority: provider.wallet.publicKey,
       },
     });
 
     await program.rpc.vote(1, {
       accounts: {
-        mainState: mainState.publicKey,
+        baseAccount: baseAccount.publicKey,
         voter: provider.wallet.publicKey,
       },
     });
 
-    const account = await program.account.mainState.fetch(mainState.publicKey);
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
 
-    console.log('Yays', account.yay);
-    console.log('Nays', account.nay);
+    console.log('Yays:', account.yay);
+    console.log('Nays:', account.nay);
+
+    // The vote is recorded.
     assert.ok(account.yay == 1);
   });
 
+  // Test 6: Removing member from whitelist
+  // ====================================================================================
+  // We will mark whitelisted2 member as inactive.
+  it('Marked as inactive: yx5...YsD', async () => {
+    console.log('========================');
+    await program.rpc.markInactive([whitelisted2], {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        authority: provider.wallet.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    const found = account.whitelist.filter(
+      (item) => item.address.toBase58() === whitelisted2.toBase58()
+    );
+
+    console.log('Unwhitelisted:', found);
+    console.log('Unwhitelisted address b58:', found[0].address.toBase58());
+
+    // Check the the whitelist length decreased from 3 to 2.
+    assert.ok(found[0].active == false);
+  });
+
+  // Test 7: Voting nay as Whitelisted
   // ***This test will fail because our provider address, which we can't change in tests, has already voted.
-  // ***Will work if we remove the has_vote check in our code. That has been verified by Otik.
-  // Test 6: Voting nay as Whitelisted
+  // ***Will work if we remove the has_vote check in our code. Verified by Otik.
   // ====================================================================================
   // it('Vote nay', async () => {
   //   console.log('========================');
   //   await program.rpc.vote(0, {
   //     accounts: {
-  //       mainState: mainState.publicKey,
+  //       baseAccount: baseAccount.publicKey,
   //       voter: provider.wallet.publicKey,
   //     },
   //   });
 
-  //   const account = await program.account.mainState.fetch(mainState.publicKey);
+  //   const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
   //   console.log('Yays', account.yay);
   //   console.log('Nays', account.nay);
   //   assert.ok(account.nay == 1);
   // });
 
+  // Test 8: Voting with a non-provider wallet in the test suite.
   // ***Testing suite limitation and will result in `Error: Signature verification failed`
-  // ***We can't run the test below because we can't change the voter argument to a non-whitelisted public key. We must sign it as the provider.wallet.publicKey which is derived from our root Keypair file on our computer system. Will skip this test for this reason.
+  // ***We can't run the test below because we can't change the voter argument to a non-whitelisted public key. We must sign it as the provider.wallet.publicKey which is derived from our root Keypair file on our computer system. Will skip this test for this reason. Verified by Otik.
   // ====================================================================================
   // it('Non-whitelist member votes yay', async () => {
   //   const NWhitelisted = anchor.web3.Keypair.generate();
 
   //   await program.rpc.vote(1, {
   //     accounts: {
-  //       mainState: mainState.publicKey,
+  //       baseAccount: baseAccount.publicKey,
   //       voter: NWhitelisted.publicKey, // We can't do this.
   //     },
   //   });
 
-  //   const account = await program.account.mainState.fetch(mainState.publicKey);
+  //   const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   //   console.log('Non-whitelist member votes yay', account);
   // });
 });
